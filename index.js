c43fe6cbd2d68774fe0a0739d285a9744ddde5f3
DanielDimitrov5 <danieldimitrov2304@gmail.com>
2023-07-02 22:54:52 +0300

    wrap ipfs client

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 import { ethers } from "ethers";
-import marketplaceABI from "./contractData/abi/NFTMarketplace.json" assert { type: 'json' };
-import nftABI from "./contractData/abi/NFT.json" assert { type: 'json' };
-import nftBytecode from "./contractData/NftBytecode.json" assert { type: 'json' };
-import marketplaceBytecode from "./contractData/MarketplaceBytecode.json" assert { type: 'json' };
+
 import axios from "axios";
-import { infuraIpfsClient } from "./ipfsClient.js";
-
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const deployNewMarketplace = async (signer, feePercent) => {
-    const factory = new ethers.ContractFactory(marketplaceABI, marketplaceBytecode.bytecode, signer);
-    const contract = await factory.deploy(feePercent);
-    await contract.deployed();
-    return contract;
-}
+import ipfsClient from "./ipfsClient.js";
 
 class NFTMarketplaceSDK {
-    constructor(signerOrProvider, contractAddress) {
+    constructor(signerOrProvider, contractAddress, marketplaceABI, nftABI, nftBytecode) {
         this.signerOrProvider = signerOrProvider;
         this.provider = signerOrProvider.provider || signerOrProvider;
         this.marketplace = {
             address: contractAddress,
             abi: marketplaceABI,
         };
+        this.nftABI = nftABI;
+        this.nftBytecode = nftBytecode;
         this.marketplaceContract = new ethers.Contract(this.marketplace.address, this.marketplace.abi, this.signerOrProvider);
     }
 
@@ -42,7 +31,7 @@ class NFTMarketplaceSDK {
             const URIPromises = items.map((item) => {
                 const nftContract = new ethers.Contract(
                     item.nftContract,
-                    nftABI,
+                    this.nftABI,
                     contract.provider,
                 );
                 const tokenUri = nftContract.tokenURI(item.tokenId.toNumber());
@@ -53,7 +42,7 @@ class NFTMarketplaceSDK {
             const URIs = await Promise.all(URIPromises);
 
             const URIsModified = URIs.map((uri) => {
-                return uri.replace('ipfs://', process.env.REACT_APP_IPFS_PROVIDER);
+                return uri.replace('ipfs://', 'https://charity-file-storage.infura-ipfs.io/ipfs/');
             });
 
             const metadataPromises = URIsModified.map((uri) => {
@@ -66,7 +55,7 @@ class NFTMarketplaceSDK {
                 return {
                     ...metadata,
                     name: metadata.data.name,
-                    image: metadata.data.image.replace('ipfs://', process.env.REACT_APP_IPFS_PROVIDER),
+                    image: metadata.data.image.replace('ipfs://', 'https://charity-file-storage.infura-ipfs.io/ipfs/'),
                     description: metadata.data.description,
                     nft: metadata.data.nft,
                     tokenId: items[metadataArr.indexOf(metadata)].tokenId,
@@ -86,13 +75,13 @@ class NFTMarketplaceSDK {
         try {
             const item = await contract.items(id);
 
-            const nftContract = new ethers.Contract(item.nftContract, nftABI, this.signerOrProvider);
+            const nftContract = new ethers.Contract(item.nftContract, this.nftABI, this.signerOrProvider);
 
-            const tokenUri = (await nftContract.tokenURI(item.tokenId)).replace('ipfs://', process.env.REACT_APP_IPFS_PROVIDER);
+            const tokenUri = (await nftContract.tokenURI(item.tokenId)).replace('ipfs://', 'https://charity-file-storage.infura-ipfs.io/ipfs/');
 
             const metadata = await axios.get(tokenUri);
 
-            metadata.data.image = metadata.data.image.replace('ipfs://', process.env.REACT_APP_IPFS_PROVIDER);
+            metadata.data.image = metadata.data.image.replace('ipfs://', 'https://charity-file-storage.infura-ipfs.io/ipfs/');
 
             return { item, metadata };
         }
@@ -114,7 +103,7 @@ class NFTMarketplaceSDK {
             const collectionContractPromises = collections.map((collection) => {
                 const collectionContract = new ethers.Contract(
                     collection,
-                    nftABI,
+                    this.nftABI,
                     this.signerOrProvider,
                 );
 
@@ -138,7 +127,7 @@ class NFTMarketplaceSDK {
     }
 
     async loadItemsForAdding(collectionAddress, owner) {
-        const contract = new ethers.Contract(collectionAddress, nftABI, this.signerOrProvider);
+        const contract = new ethers.Contract(collectionAddress, this.nftABI, this.signerOrProvider);
 
         const itemsMarketplaceItemsIds = (await this.loadItems()).items.filter((item) => {
             return item.nftContract === collectionAddress;
@@ -163,7 +152,7 @@ class NFTMarketplaceSDK {
         });
 
         const URIs = (await Promise.all(promises)).map((uri) => {
-            return uri.replace('ipfs://', process.env.REACT_APP_IPFS_PROVIDER);
+            return uri.replace('ipfs://', 'https://charity-file-storage.infura-ipfs.io/ipfs/');
         });
 
         const metadataPromises = URIs.map((uri) => {
@@ -176,7 +165,7 @@ class NFTMarketplaceSDK {
             return {
                 ...metadata,
                 name: metadata.data.name,
-                image: metadata.data.image.replace('ipfs://', process.env.REACT_APP_IPFS_PROVIDER),
+                image: metadata.data.image.replace('ipfs://', 'https://charity-file-storage.infura-ipfs.io/ipfs/'),
                 description: metadata.data.description,
                 nft: metadata.data.nft,
                 tokenId: ids[i]
@@ -189,23 +178,23 @@ class NFTMarketplaceSDK {
     async getItemsTokenIds(contract) {
         const eventFilter = contract.filters.Transfer([ethers.constants.AddressZero]);
         const events = await contract.queryFilter(eventFilter);
-    
+
         const arrIds = events.map((event) => {
             return event.args.tokenId.toNumber();
         });
-    
+
         return arrIds;
     }
 
     async addItemToMarketplace(collectionAddress, tokenId) {
         try {
             const contract = this.marketplaceContract;
-    
+
             const eventFilter = contract.filters.LogCollectionAdded();
             const collectionid = (await contract.queryFilter(eventFilter)).filter((event) => event.args.nftCollection === collectionAddress)[0].args.id;
-    
+
             const transaction = await contract.addItem(collectionid, tokenId, { gasLimit: 300000 });
-    
+
             const tx = await transaction.wait();
             return tx.status;
         } catch (error) {
@@ -216,15 +205,15 @@ class NFTMarketplaceSDK {
     async loadItemsForListing(owner) {
         try {
             const { items, metadataArrModified } = await this.loadItems();
-    
+
             const filteredItems = items.filter(item => item.owner === owner && parseInt(item.price) === 0);
-    
+
             const nfts = metadataArrModified.filter((item) => {
                 return filteredItems.some((item2) => {
                     return parseInt(item.tokenId) === parseInt(item2.tokenId) && item.nft === item2.nftContract;
                 });
             });
-    
+
             return { filteredItems, nfts };
         } catch (error) {
             console.log(error);
@@ -234,21 +223,21 @@ class NFTMarketplaceSDK {
     async listItemForSale(collectionAddress, tokenId, price) {
         try {
             const isApproved = await this.checkApproval(collectionAddress, tokenId);
-    
+
             if (isApproved !== this.marketplace.address) {
                 await this.approveToken(collectionAddress, tokenId);
             }
 
             const contract = this.marketplaceContract;
-    
+
             const items = await this.loadItems();
-    
+
             const itemId = parseInt(items.items.filter(item => item.nftContract === collectionAddress && parseInt(item.tokenId) === parseInt(tokenId))[0].id);
-    
+
             const transaction = await contract.listItem(itemId, price, { gasLimit: 300000 });
-    
+
             const tx = await transaction.wait();
-    
+
             return tx.status;
         } catch (error) {
             console.log(error);
@@ -257,10 +246,10 @@ class NFTMarketplaceSDK {
 
     async checkApproval(collectionAddress, tokenId) {
         try {
-            const contract = new ethers.Contract(collectionAddress, nftABI, this.signerOrProvider);
-    
+            const contract = new ethers.Contract(collectionAddress, this.nftABI, this.signerOrProvider);
+
             const approved = await contract.getApproved(tokenId);
-    
+
             return approved;
         } catch (error) {
             console.log(error);
@@ -269,12 +258,12 @@ class NFTMarketplaceSDK {
 
     async approveToken(collectionAddress, tokenId) {
         try {
-            const contract = new ethers.Contract(collectionAddress, nftABI, this.signerOrProvider);
-    
+            const contract = new ethers.Contract(collectionAddress, this.nftABI, this.signerOrProvider);
+
             const transaction = await contract.approve(this.marketplace.address, tokenId, { gasLimit: 300000 });
-    
+
             const tx = await transaction.wait();
-    
+
             return tx.status;
         } catch (error) {
             console.log(error);
@@ -284,79 +273,83 @@ class NFTMarketplaceSDK {
     async buyItem(itemId, price) {
         try {
             const contract = this.marketplaceContract;
-    
+
             const transaction = await contract.buyItem(itemId, { value: price, gasLimit: 300000 });
-    
+
             const tx = await transaction.wait();
-    
+
             return tx.status;
         } catch (error) {
             console.log(error);
         }
     }
 
-    async addExistingCollection(collectionAddress){
+    async addExistingCollection(collectionAddress) {
         try {
             const contract = this.marketplaceContract;
-    
+
             const transaction = await contract.addCollection(collectionAddress, { gasLimit: 300000 });
-    
+
             const tx = await transaction.wait();
-    
+
             return tx.status;
         } catch (error) {
             console.log(error);
         }
     }
 
-    async mintNFT(collectionAddress, metadata) {
 
-        const { image } = metadata;
-    
-        let imageHash = 'ipfs://' + "QmcZcBrDxzXogmpGoh1jU3AjSjGTqHVoM4pNtejAEeMc5J";
-    
-        if (image) {
-            imageHash = await this.uploadToIPFS(image);
-        }
-    
-        metadata.image = imageHash;
-        metadata.nft = collectionAddress;
-    
-        try {
-            const contract = new ethers.Contract(collectionAddress, nftABI, this.signerOrProvider);
-    
-            const metadataURI = await this.uploadToIPFS(JSON.stringify(metadata));
-    
-            const transaction = await contract.mint(metadataURI, { gasLimit: 300000 });
-    
-            const tx = await transaction.wait();
-    
-            return tx.status;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    
-    async uploadToIPFS(data) {
-        try {
-            const addedFile = await infuraIpfsClient.add(data);
-            const hash = addedFile.path;
-    
-            return 'ipfs://' + hash;
-        } catch (err) {
-            console.log(err);
-            return;
+    infuraIpfsClient(projectId, projectSecret) {
+
+        const client = new ipfsClient(projectId, projectSecret);
+
+        const nftABI = this.nftABI;
+        const signerOrProvider = this.signerOrProvider;
+
+        return {
+            async uploadToIPFS(file) {
+                const added = await client.addFile(file);
+
+                return "ipfs://" + added;
+            },
+
+            async mintNFT (collectionAddress, metadata) {
+                const { image } = metadata;
+
+                let imageHash = 'ipfs://' + "QmcZcBrDxzXogmpGoh1jU3AjSjGTqHVoM4pNtejAEeMc5J";
+
+                if (image) {
+                    imageHash = await this.uploadToIPFS(image);
+                }
+
+                metadata.image = imageHash;
+                metadata.nft = collectionAddress;
+
+                try {
+                    const contract = new ethers.Contract(collectionAddress, nftABI, signerOrProvider);
+
+                    const metadataURI = await this.uploadToIPFS(JSON.stringify(metadata));
+
+                    const transaction = await contract.mint(metadataURI, { gasLimit: 300000 });
+
+                    const tx = await transaction.wait();
+
+                    return tx.status;
+                } catch (error) {
+                    console.log(error);
+                }
+            }
         }
     }
 
     async placeOffer(itemId, price) {
         try {
             const contract = this.marketplaceContract;
-    
+
             const transaction = await contract.placeOffer(itemId, price, { gasLimit: 300000 });
-    
+
             const tx = await transaction.wait();
-    
+
             return tx.status;
         } catch (error) {
             console.log(error);
@@ -366,15 +359,15 @@ class NFTMarketplaceSDK {
     async getOffers(itemId) {
         try {
             const contract = this.marketplaceContract;
-    
+
             const offerers = [...new Set(await contract.getOfferers(itemId))];
-    
+
             const offerssPromises = offerers.map(async (offerer) => {
                 return contract.offers(itemId, offerer);
             });
-    
+
             const offers = await Promise.all(offerssPromises);
-    
+
             const offersModified = offers.map((offer, i) => {
                 return {
                     offerer: offerers[i],
@@ -383,7 +376,7 @@ class NFTMarketplaceSDK {
                     seller: offer.seller
                 }
             });
-    
+
             return offersModified;
         } catch (error) {
             console.log(error);
@@ -396,69 +389,69 @@ class NFTMarketplaceSDK {
             const item = (await this.getItem(itemId)).item;
             const nftContractAddress = item.nftContract;
             const tokenId = item.tokenId;
-    
+
             const approve = await this.checkApproval(nftContractAddress, tokenId);
-    
+
             if (approve != this.marketplace.address) {
                 const aprrovalTx = await this.approveToken(nftContractAddress, tokenId);
-    
+
                 if (aprrovalTx !== 1) {
                     alert('Approval failed');
                     return;
                 }
             }
-    
+
             const contract = this.marketplaceContract;
-    
+
             const transaction = await contract.acceptOffer(itemId, offerer, { gasLimit: 300000 });
-    
+
             const tx = await transaction.wait();
-    
+
             return tx.status;
         } catch (error) {
             console.log(error);
         }
     }
 
-    async getAccountsOffers(address)  {
+    async getAccountsOffers(address) {
         try {
             const contract = this.marketplaceContract;
-    
+
             const itemCount = await contract.itemCount();
-    
+
             const itemCountArr = [...Array(parseInt(itemCount)).keys()].map(i => i + 1);
-    
+
             const getOffersPromises = itemCountArr.map(async (id) => {
                 const offer = contract.offers(id, address);
                 return offer;
             });
-    
+
             const offers = (await Promise.all(getOffersPromises)).filter(offer => offer.itemId.toNumber() !== 0);
-    
+
             const itemIds = offers.map(offer => offer.itemId.toNumber());
-    
+
             const itemsPromises = itemIds.map(async (id) => {
                 const item = contract.items(id);
                 return item;
             });
-    
+
             const items = (await Promise.all(itemsPromises));
-    
+
             const itemsOwners = items.map(item => item.owner);
-    
+
             const offersModified = offers.filter((offer, i) => offer.seller === itemsOwners[i] && items[i].price.toNumber() === 0);
-    
+
             return offersModified;
         }
         catch (error) {
             console.log(error);
         }
     }
-    
+
     async getOffer(itemId, offerer) {
         try {
             const contract = this.marketplaceContract;
-    
+
             const offer = await contract.offers(itemId, offerer);
             return offer;
         }
@@ -470,23 +463,23 @@ class NFTMarketplaceSDK {
     async claimItem(itemId, price) {
         try {
             const contract = this.marketplaceContract;
-    
+
             const transaction = await contract.claimItem(itemId, { value: price, gasLimit: 300000 });
-    
+
             const tx = await transaction.wait();
-    
+
             return tx.status;
         } catch (error) {
             console.log(error);
         }
     }
-    
+
     async isMarketplaceOwner(address) {
         try {
             const contract = this.marketplaceContract;
-    
+
             const owner = await contract.owner();
-    
+
             return owner === address;
         } catch (error) {
             console.log(error);
@@ -496,11 +489,11 @@ class NFTMarketplaceSDK {
     async withdrawMoney() {
         try {
             const contract = this.marketplaceContract;
-    
+
             const transaction = await contract.withdraw({ gasLimit: 300000 });
-    
+
             const tx = await transaction.wait();
-    
+
             return tx.status;
         } catch (error) {
             console.log(error);
@@ -510,7 +503,7 @@ class NFTMarketplaceSDK {
     async getMarketplaceBalance() {
         try {
             const balance = await this.provider.getBalance(this.marketplace.address);
-    
+
             return balance;
         } catch (error) {
             console.log(error);
@@ -519,26 +512,26 @@ class NFTMarketplaceSDK {
 
     async deployNFTCollection(name, symbol) {
         try {
-            const factory = new ethers.ContractFactory(nftABI, nftBytecode.bytecode, this.signerOrProvider);
-    
+            const factory = new ethers.ContractFactory(this.nftABI, this.nftBytecode, this.signerOrProvider);
+
             const contract = await factory.deploy(name, symbol);
-    
+
             await contract.deployed();
-    
+
             return contract;
         } catch (error) {
             console.log(error);
         }
     }
 
-    async approveToken (collectionAddress, tokenId) {
+    async approveToken(collectionAddress, tokenId) {
         try {
-            const contract = new ethers.Contract(collectionAddress, nftABI, this.signerOrProvider);
-    
+            const contract = new ethers.Contract(collectionAddress, this.nftABI, this.signerOrProvider);
+
             const transaction = await contract.approve(this.marketplace.address, tokenId, { gasLimit: 300000 });
-    
+
             const tx = await transaction.wait();
-    
+
             return tx.status;
         } catch (error) {
             console.log(error);
--- ipfsClient.js
+++ ipfsClient.js
@@ -1,20 +1,41 @@
 import { Buffer } from 'buffer';
 import { create } from "ipfs-http-client";
 
-import dotenv from 'dotenv';
-dotenv.config();
+const projectId = '2JgRgaB0c4gtdx2UnSOT0gnODyU';
+const projectSecret = "3ccaa9dce7ddd7051d208e7d00ae5eb4";
 
-const projectId = process.env.REACT_APP_PROJECT_ID
-const projectSecret = process.env.REACT_APP_PROJECT_SECRET;
-
-const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
-
-export const infuraIpfsClient = create({
-    host: 'ipfs.infura.io',
-    port: 5001,
-    protocol: 'https',
-    apiPath: '/api/v0',
-    headers: {
-        authorization: auth,
+class ipfsClient {
+    
+    constructor(projectId, projectSecret) {
+        const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+        
+        this.ipfs = create({
+            host: 'ipfs.infura.io',
+            port: 5001,
+            protocol: 'https',
+            apiPath: '/api/v0',
+            headers: {
+                authorization: auth,
+            }
+        });
     }
-})
+
+    async addFile(file) {
+        const added = await this.ipfs.add(file);
+        return added.path;
+    }
+}
+
+export default ipfsClient;
+
+// const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+
+// export const infuraIpfsClient = create({
+//     host: 'ipfs.infura.io',
+//     port: 5001,
+//     protocol: 'https',
+//     apiPath: '/api/v0',
+//     headers: {
+//         authorization: auth,
+//     }
+// })
